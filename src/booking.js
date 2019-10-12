2610f731acc43062049b205252152b3e97502ed0
ttolentino89 <trishatolentino89@gmail.com>
2019-10-12 15:53:00 -0400

    added success modal, event listeners
    added success modal, event listeners

---
+++ src/booking.js
@@ -0,0 +1,12 @@
+function success() {
+    var x = document.getElementById("rightMenu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+// function clearText() {
+//     document.getElementById("largerText").value = "";
+// }
